be79a7d9f37cbbf8673beb2d84a2830183e6e851
Mangeshbadgujar123 <mangeshbadgujar123@gmail.com>
2021-11-27 12:43:45 +0530

    changes added

--- SpringBootFindDataByAnyInput/src/main/java/com/csi/controller/ProductController.java
+++ SpringBootFindDataByAnyInput/src/main/java/com/csi/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.csi.model.Product;
 import com.csi.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,5 +31,9 @@ public class ProductController {
     public ResponseEntity <List<Product>> getDataByAnyInput(@RequestBody Product product)
     {
         return ResponseEntity.ok(productService.getAllData(product));
+    }
+    @GetMapping("/bye")
+    public String byeWorld(){
+    	return "bye bye kajal Maner!!!";
     }
 }
